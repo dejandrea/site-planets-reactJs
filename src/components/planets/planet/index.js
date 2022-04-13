4e21f660bdfe1aebbe38a0ef11501e2feb003787
Andréa J S França <dejandrea@gmail.com>
2022-04-12 23:27:02 -0300

    renderizando vários elementos

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -5,6 +5,11 @@ import GrayImg from "../../shared/gray_img";
 import DescriptionWithLink from './../../shared/description_with_link/index';
 
 const Planet = (props) => {
+    //renderizando vários elementos fara do JSX
+    //const names = ['a','b','c','d']
+    // const satellites = names.map((n)=>
+    //     <li>Satélite {n}</li>
+    // )
     let title;
     if (props.title_with_underline) {
         title = <h4><u>{props.name}</u></h4>
@@ -17,6 +22,17 @@ const Planet = (props) => {
             {title}
             <DescriptionWithLink description={props.description} link={props.link} />
             <GrayImg img_url={props.img_url} gray={props.gray}/>
+            <h4>Satélites</h4>
+            {/* <ul>
+                {satellites}
+            </ul> */}
+            {/* renderizando varios elementos no JSX */}
+            <ul>
+                {['a','b','c','d','e'].map((n)=>
+                <li>satélite {n}</li>
+                )}
+            </ul>
+            <hr/>
         </div>
    ) 
 }
