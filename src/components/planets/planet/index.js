ee7ca9ca366748c80f0c9c107ac333a1037301e2
Andréa J S França <dejandrea@gmail.com>
2022-04-23 18:45:48 -0300

    -Hooks

--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useState, useEffect }from "react";
 //import LoremIpsum from "../../loremIpsum";
 import GrayImg from "../../shared/gray_img";
 //import DescriptionWithLink from "../../shared/description_with_link";
@@ -10,54 +10,38 @@ async function getSatellites(id) {
     return data
 }
 
-class Planet extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            satellites: []
-        }
-    }
-    componentDidMount() {
-        getSatellites(this.props.id).then(data => {
-            this.setState(state => ({
-                satellites: data['satellites']
-            }))
+const Planet = (props) => {
+    const [satellites, setSatellites] = useState([])
+
+    useEffect(() => {
+        getSatellites(props.id).then(data => {
+            setSatellites(data['satellites'])
         })
+    },[])
+
+
+
+    let title;
+    if (props.title_with_underline) {
+        title = <h4><u>{props.name}</u></h4>
+    } else {
+        title = <h4>{props.name}</h4>
     }
-    render() {
 
-
-        let title;
-        if (this.props.title_with_underline) {
-            title = <h4><u>{this.props.name}</u></h4>
-        } else {
-            title = <h4>{this.props.name}</h4>
-        }
-
-        return (
-            <div onClick={() => this.props.clickOnPlanet(this.props.name)}>
-                {title}
-                <DescriptionWithLink description={this.props.description} link={this.props.link} />
-                <GrayImg img_url={this.props.img_url} gray={this.props.gray} />
-                <h4>Satélites</h4>
-                <ul>
-                    {this.state.satellites.map((satellite, index) =>
-                        <li key={index}>{satellite.name}</li>
-                    )}
-                </ul>
-                {/* <ul>
-                {satellites}
-            </ul> */}
-                {/* renderizando varios elementos no JSX */}
-                {/* <ul>
-                {['a','b','c','d','e'].map((n)=>
-                <li>satélite {n}</li>
+    return (
+        <div onClick={() => props.clickOnPlanet(props.name)}>
+            {title}
+            <DescriptionWithLink description={props.description} link={props.link} />
+            <GrayImg img_url={props.img_url} gray={props.gray} />
+            <h4>Satélites</h4>
+            <ul>
+                {satellites.map((satellite, index) =>
+                    <li key={index}>{satellite.name}</li>
                 )}
-            </ul> */}
-                <hr />
-            </div>
-        )
-    }
+            </ul>
+            <hr />
+        </div>
+    )
 }
 
 export default Planet;
